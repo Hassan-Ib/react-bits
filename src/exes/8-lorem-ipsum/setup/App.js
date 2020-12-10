f62d474e8ec4b2abc34af3e78656693990e50b22
Hassan-Ib <azanebrahim@gmail.com>
2020-12-10 10:10:41 -0500

    useRef is awesome

--- src/exes/8-lorem-ipsum/setup/App.js
+++ src/exes/8-lorem-ipsum/setup/App.js
@@ -1,11 +1,55 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState, useRef } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const refcontainer = useRef(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refcontainer.current.value);
+    console.log(data.slice(0, count));
+    let newText = [];
+    let amount = count;
+    if (count <= 0) {
+      amount = 1;
+    }
+    for (let i = 0; i <= amount - 1; ) {
+      const index = Math.floor(Math.random() * data.length);
+      newText.push(data[index]);
+      i += 1;
+    }
+    setText(newText);
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form action="#" className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          ref={refcontainer}
+          name="amount"
+          id="amount"
+          value={count}
+          min="0"
+          max="50"
+          onChange={(e) => {
+            // setCount(e.target.value);
+            setCount(refcontainer.current.value);
+          }}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // import App from "./workOut/app";
-import "./exes/8-lorem-ipsum/setup/index.css"; // importing the css
-import App from "./exes/8-lorem-ipsum/setup/App"; // importing the app js
+import "./exes/9-color-generator/setup/index.css"; // importing the css
+import App from "./exes/9-color-generator/setup/App"; // importing the app js
 
 // import "./exes/7-slider/setup/index.css"; // importing the css
 // import App from "./exes/7-slider/setup/App"; // importing the app js
